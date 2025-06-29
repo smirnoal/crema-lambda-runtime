839aa6a89b71aa95c5e6a1a68bd29103ed522ba5
Alexander Smirnov <alexander.smirnoff@gmail.com>
2025-06-29 12:44:13 +0100

    29 June 2025 - fix comments

--- common/src/main/java/com/smirnoal/lambda/serde/LambdaSerde.java
+++ common/src/main/java/com/smirnoal/lambda/serde/LambdaSerde.java
@@ -4,14 +4,14 @@ import java.util.function.Function;
 
 public interface LambdaSerde<T, R> {
     /**
-     * Function to create input object from byte array
-     * @return object of input type
+     * Function to create an input object from a byte array.
+     * @return an object of the input type
      */
     Function<byte[], T> inputDeserializer();
 
     /**
-     * Function to create byte array from an output object
-     * @return byte array
+     * Function to create a byte array from an output object.
+     * @return a byte array
      */
     Function<R, byte[]> outputSerializer();
 }
